c55ae17c33c8bad24e3808a44daea2535387275d
karmaKiller3352 <alievsul@gmail.com>
2020-07-14 00:52:47 +0300

    deploy

---
+++ precache-manifest.71ce3e9508fa827869e34e111dc012ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1590bd9a12871e2fe617cd4ea1c02858",
+    "url": "/memorize-game/index.html"
+  },
+  {
+    "revision": "ab4252e570287167d98d",
+    "url": "/memorize-game/static/css/main.b6d45c51.chunk.css"
+  },
+  {
+    "revision": "9a99a62839e8b63aa003",
+    "url": "/memorize-game/static/js/2.717e5194.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memorize-game/static/js/2.717e5194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab4252e570287167d98d",
+    "url": "/memorize-game/static/js/main.64cead04.chunk.js"
+  },
+  {
+    "revision": "bc1ab03655d97ae9eacd",
+    "url": "/memorize-game/static/js/runtime-main.d9b31247.js"
+  }
+]);
